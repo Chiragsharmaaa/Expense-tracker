79f08c1ad97b65649d0af5a9572d3445f61b31f0
Chiragsharma <chiragsharma250999@gmail.com>
2022-12-14 22:22:40 +0530

    hashed password using bcrypt

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcrypt');
+
 const User = require('../models/user');
 
 exports.postSignup = async (req, res, next) => {
@@ -6,29 +8,48 @@ exports.postSignup = async (req, res, next) => {
         const user = await User.findAll({ where: { email } });
         if (user.length > 0) {
             return res.status(550).json({ message: 'user already exists!' });
-        }
-        await User.create({ name, email, password });
-        return res.status(201).json({ message: 'user created!' });
+        };
+
+        const saltRounds = 10;
+
+        bcrypt.hash(password, saltRounds, async (err, hash) => {
+            console.log(err)
+            await User.create({ name, email, password: hash });
+            return res.status(201).json({ message: 'user created!' });
+        });
+
     } catch (err) {
         res.status(500).json(err);
     };
 };
-
 exports.postLogin = async (req, res, next) => {
     try {
+
         const { email, password } = req.body;
-        const user = await User.findAll({ where: { email } });
-        if (user.length == 0) {
-            return res.status(403).json({ message: `user doesn't exists!` })
+
+        if (!email || !password) {
+            return res.status(400).json({ message: 'Enter all fields' })
         }
 
+        const user = await User.findAll({ where: { email } })
+
+        if (user.length === 0) {
+            return res.status(404).json({ message: 'User not found' })
+        }
         const existingUser = user[0];
 
-        if (existingUser.password != password) {
-            return res.status(401).json({ message: 'Please check your password!' })
-        };
-        return res.status(200).json(user);
-    } catch(error) {
-        return res.status(500).json(error);
+        bcrypt.compare(password, existingUser.password, (err, result) => {
+            if (err) {
+                return new Error('Something went wrong')
+            };
+            if (result === true) {
+                return res.status(200).json({ success: true, message: 'User logged in successfully!' });
+            } else {
+                return res.status(400).json({ success: false, message: 'Password is incorrect' })
+            }
+        });
+
+    } catch (err) {
+        return res.status(500).json({ message: err, success: false })
     }
 }
